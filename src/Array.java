3d9c0d420b1f9d60799d5efae55835a808b843fe
heyren66666 <1873714818@qq.com>
2019-10-12 15:32:14 +0800

    数组2.0

--- src/Array.java
+++ src/Array.java
@@ -1,3 +1,47 @@
 public class Array {
+    private String [] strArray;
+    private int length=0;
 
+    //构造方法
+    public Array(int max){
+        strArray=new String [max];
+    }
+    //检测是否包含某个元素
+    public int contains(String target){
+        int index=-1;
+        for (int i=0;i<length;i++){
+            if (strArray[i].equals(target)){
+                index=i;
+                break;
+            }
+        }
+        return index;
+    }
+
+    //插入
+    public void insert(String elem){
+        strArray[length]=elem;
+        length++;
+    }
+
+    //删除某个指定元素值，删除成功则返回true，否则返回false
+    public boolean delete(String target){
+        int index=-1;
+        if ((index=contains(target))!=-1){
+            for (int i=index;i<length;i++){
+                strArray[i]=strArray[i+1];
+            }
+            length--;
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    //列出所有元素
+    public void display(){
+        for (int i=0;i<length;i++){
+            System.out.print(strArray[i]+"\t");
+        }
+    }
 }
