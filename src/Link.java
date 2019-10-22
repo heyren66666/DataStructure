c6dcb5698a2238a851943525de6ce63f14dfcd21
heyren66666 <1873714818@qq.com>
2019-10-22 15:33:50 +0800

    单链表1.0

---
+++ src/Link.java
@@ -0,0 +1,16 @@
+public class Link {
+    public int age;
+    public String name;
+    public Link next;//指向该链结点的下一个链结点
+
+    //构造方法
+    public Link(int age,String name){
+        this.age=age;
+        this.name=name;
+    }
+
+    //打印该链结点的信息
+    public void displayLink(){
+        System.out.println("name:"+name+",age:"+age);
+    }
+}
