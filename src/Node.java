b5265968e627c161c03049d899beac5ff6fc3225
heyren66666 <1873714818@qq.com>
2019-10-23 11:46:55 +0800

    二叉树结点1.0

---
+++ src/Node.java
@@ -0,0 +1,17 @@
+//树节点的封装类
+public class Node {
+    int age;
+    String name;
+    Node leftChild;//左子节点的引用
+    Node rightChild;//右子节点的引用
+
+    public Node(int age,String name){
+        this.age=age;
+        this.name=name;
+    }
+
+    //打印该节点的信息
+    public void dispalyNode(){
+        System.out.println("name:"+name+",age"+age);
+    }
+}
